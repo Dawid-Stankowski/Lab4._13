0a34277a121744ef3e179deb7764f64ddc592aeb
Dawid-Stankowski <dawid441@interia.pl>
2023-12-15 09:13:43 +0100

    1.0

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    // Pola klasy Car
+    private String brand;
+    private String model;
+    private int year;
+    private int numberOfDoors;
+    private double engineCapacity;
+    private double price;
+
+    public Car(String brand, String model, int year, int numberOfDoors, double engineCapacity, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.numberOfDoors = numberOfDoors;
+        this.engineCapacity = engineCapacity;
+        this.price = price;
+    }
+    // Metoda wyświetlająca informacje o samochodzie
+    public void displayCarInfo() {
+        System.out.println("Marka: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Rok produkcji: " + year);
+        System.out.println("Liczba drzwi: " + numberOfDoors);
+        System.out.println("Pojemność silnika: " + engineCapacity);
+        System.out.println("Cena: " + price);
+        System.out.println("-------------");
+    }
 }
--- src/TestCar.java
+++ src/TestCar.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class TestCar {
+public class TestCar {
+    public static void main(String[] args) {
+        Car car1 = new Car("Suzuki", "Grand Vitara", 2012, 5, 1.9, 25000);
+        Car car2 = new Car("Ford", "Focus", 2021, 5, 2.0, 28000);
+        Car car3 = new Car("Toyota", "Rav4", 2015, 5, 1.5, 22000);
+
+        Car[] cars = {car1, car2, car3};
+        for (Car car : cars) {
+            car.displayCarInfo();
+        }
+    }
 }
